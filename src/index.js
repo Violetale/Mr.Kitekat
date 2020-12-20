eaeec67c609baa366b2d20c33ad07eaf870a104d
Svetlana Dmitrieva <53534598+Violetale@users.noreply.github.com>
2020-12-20 14:28:07 +0300

    Added setup for webpack.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import $ from 'jquery';
 import 'normalize.css';
 
 import './assets/scss/fonts.scss';
-import './assets/scss/reset.scss'
+import './assets/scss/reset.scss';
 
-import './pages/index.pug
-import './js/app'
+import './pages/index.js';
+
+import './js/app';
